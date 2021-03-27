78500dc44c4a3572c5a4cdb5a54fac2b7bc96a0e
Miguel Nava <miguel@devhive.mx>
2021-03-27 00:20:33 -0600

    config auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -77,6 +77,7 @@ const loginUsuario = async (req, res) => {
       token
     })
   } catch(error) {
+    console.log('error => ', error);
     res.status(500).json({
       ok: false,
       message: 'Contacte con el administrador'
