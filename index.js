beffdc35f3c4113a201bc59f012ea620616f9e04
Miguel Nava <miguel@devhive.mx>
2021-03-27 02:40:11 -0600

    finall version

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ app.set('port', process.env.PORT || 3000);
 // Rutas
 app.use('/api/auth', router);
 
-
 // Manejar demas rutas
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public/index.html'));
